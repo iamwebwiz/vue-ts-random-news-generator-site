24f5731f6af6ce34e2f4fadb4b0bcdc46df77974
Ezekiel <iamwebwiz@gmail.com>
2020-09-12 16:01:26 +0100

    update

--- src/my-reader-root.vue
+++ src/my-reader-root.vue
@@ -62,75 +62,90 @@
 </template>
 
 <script lang="ts">
-  import Vue from "vue";
+  import { Component, Vue } from "vue-property-decorator";
 
-  export default Vue.extend({
-    name: "App",
-    data() {
-      return {
-        appName: "Random news site generator",
-        alreadyRead: [],
-        toRead: [],
-        selected_article: null,
-      };
+  interface ArticleType {
+    title: string;
+    url: string;
+    abstract: string;
+  }
+
+  @Component({
+    mounted(): void {
+      console.log("Okay, please work!");
     },
-    components: {
-      //
-    },
-    computed: {
-      readingStatus() {
-        if (this.alreadyRead.length == 0 && this.toRead.length == 0)
-          return "Add something to read to get the show started";
-        if (this.alreadyRead.length > 0 && this.toRead.length == 0)
-          return "Was that it? Add more below";
-        if (this.toRead.length == 0) return "Get reading!";
-        return "hmm - this is strange";
-      },
-    },
-    methods: {
-      // add a new article to the reading list
-      func6() {
-        //include depedencny
-        const axios = require("axios");
-        // create get call to grab list of potential article soruces
-        try {
-          axios.get("/test-data.json").then((response) => {
-            // response
-            let res = response;
-            // data from response
-            let data = res.data;
-            // make random number
-            let r = Math.max(Math.floor(Math.random() * 5) - 1, 0);
-            // get random article
-            let ra = data.results[r];
-            // update the state
-            this.toRead.push(ra);
-          });
-        } catch (error) {
-          alert(error);
-        }
-      },
-      func5(article) {
-        try {
-          return this.selected_article.title == article.title;
-        } catch (exception) {
-          return false;
-        }
-      },
-      func1(article) {
-        return (this.selected_article = article);
-      },
-      func2(article) {
-        if (this.selected_article.title == article.title)
-          this.selected_article = null;
-      },
-      func3(ar) {
-        this.alreadyRead.push(ar);
-        let i = this.toRead.findIndex(function(a) {
-          return a.url == ar.url;
-        });
-        this.toRead.splice(i, 1);
-      },
-    },
-  });
+  })
+  export default class App extends Vue {
+    private appName: string = "Random news site generator";
+    private read: Array<ArticleType> = [];
+  }
+
+  // export default Vue.extend({
+  //   name: "App",
+  //   data() {
+  //     return {
+  //       appName: "Random news site generator",
+  //       alreadyRead: [],
+  //       toRead: [],
+  //       selected_article: null,
+  //     };
+  //   },
+  //   components: {
+  //     //
+  //   },
+  //   computed: {
+  //     readingStatus() {
+  //       if (this.alreadyRead.length == 0 && this.toRead.length == 0)
+  //         return "Add something to read to get the show started";
+  //       if (this.alreadyRead.length > 0 && this.toRead.length == 0)
+  //         return "Was that it? Add more below";
+  //       if (this.toRead.length == 0) return "Get reading!";
+  //       return "hmm - this is strange";
+  //     },
+  //   },
+  //   methods: {
+  //     // add a new article to the reading list
+  //     func6() {
+  //       //include depedencny
+  //       const axios = require("axios");
+  //       // create get call to grab list of potential article soruces
+  //       try {
+  //         axios.get("/test-data.json").then((response: any) => {
+  //           // data from response
+  //           let data = response.data;
+  //           console.log(data);
+  //           // make random number
+  //           let r = Math.max(Math.floor(Math.random() * 5) - 1, 0);
+  //           // get random article
+  //           let ra = data.results[r];
+  //           // update the state
+  //           this.toRead.push(ra);
+  //         });
+  //       } catch (error) {
+  //         alert(error);
+  //       }
+  //     },
+  //     func5(article) {
+  //       try {
+  //         return this.selected_article.title == article.title;
+  //       } catch (exception) {
+  //         return false;
+  //       }
+  //     },
+  //     func1(article) {
+  //       return (this.selected_article = article);
+  //     },
+  //     func2(article) {
+  //       if (this.selected_article.title == article.title)
+  //         this.selected_article = null;
+  //     },
+  //     func3(ar) {
+  //       this.alreadyRead.push(ar);
+  //       let i = this.toRead.findIndex(function(a) {
+  //         return a.url == ar.url;
+  //       });
+  //       this.toRead.splice(i, 1);
+  //     },
+  //   },
+  // });
 </script>
