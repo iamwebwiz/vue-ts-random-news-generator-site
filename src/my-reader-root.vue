9f305d1bc1e5bce8bc3e7b8fa152056025fa144b
Ezekiel <iamwebwiz@gmail.com>
2020-09-12 18:26:29 +0100

    update

--- src/my-reader-root.vue
+++ src/my-reader-root.vue
@@ -13,14 +13,18 @@
               Status:
               <span class="font-bold text-gray-800">{{ readingStatus }}</span>
             </div>
-            <div v-if="toRead.length > 0">
+            <div v-if="unreadArticles.length > 0">
               <h2>Reading list:</h2>
               <div
-                v-for="(article, index) in toRead"
+                v-for="(article, index) in unreadArticles"
                 :key="index"
                 class="bg-gray-200 px-4 py-2 my-2 rounded shadow"
               >
-                <div @mouseenter="func1(article)" @mouseleave="func2(article)">
+                <div
+                  @mouseenter="setSelectedArticle(article)"
+                  @mouseleave="unsetSelectedArticle(article)"
+                  @click="toggleShowArticleAbstract(article)"
+                >
                   <div class="flex">
                     <span class="text-grey-600">Name:</span>
                     <span class="font-bold text-gray-900">
@@ -31,7 +35,7 @@
                   <div>
                     <button
                       class="bg-red-400 text-white py-1 px-3 rounded-full font-bold shadow my-3"
-                      @click="func3(article)"
+                      @click="markAsRead(article, index)"
                     >
                       Read it
                     </button>
@@ -42,7 +46,7 @@
                       >Open</a
                     >
                   </div>
-                  <div v-if="func5(article)" class="bg-gray-400 p-4">
+                  <div v-if="article.showAbstract" class="bg-gray-400 p-4">
                     Abstract: {{ article.abstract }}
                   </div>
                 </div>
@@ -50,7 +54,7 @@
             </div>
             <button
               class="bg-red-400 text-white py-1 px-3 rounded-full font-bold shadow my-3"
-              @click.prevent="func6"
+              @click.prevent="fetchRandomArticle"
             >
               Add reading
             </button>
@@ -63,21 +67,69 @@
 
 <script lang="ts">
   import { Component, Vue } from "vue-property-decorator";
+  import axios from "axios";
 
   interface ArticleType {
     title: string;
     url: string;
     abstract: string;
+    showAbstract: boolean;
   }
 
-  @Component({
-    mounted(): void {
-      console.log("Okay, please work!");
-    },
-  })
+  @Component({})
   export default class App extends Vue {
     private appName: string = "Random news site generator";
-    private read: Array<ArticleType> = [];
+    private readArticles: Array<ArticleType> = [];
+    private unreadArticles: Array<ArticleType> = [];
+    private selectedArticle?: ArticleType | null = null;
+
+    get readingStatus(): string {
+      if (this.readArticles.length == 0 && this.unreadArticles.length == 0)
+        return "Add something to read to get the show started";
+      if (this.readArticles.length > 0 && this.unreadArticles.length == 0)
+        return "Was that it? Add more below";
+      if (this.unreadArticles.length == 0) return "Get reading!";
+      return "hmm - this is strange";
+    }
+
+    public fetchRandomArticle() {
+      // const axios = require("axios");
+      // create get call to grab list of potential article soruces
+      try {
+        axios.get("/test-data.json").then((response: any) => {
+          // data from response
+          let data = response.data;
+          // make random number
+          let r = Math.max(Math.floor(Math.random() * 5) - 1, 0);
+          // get random article
+          let ra = data.results[r];
+          // update the state
+          this.unreadArticles.push(ra);
+        });
+      } catch (error) {
+        alert(error);
+      }
+    }
+
+    public toggleShowArticleAbstract(article: ArticleType): void {
+      article.showAbstract = !article.showAbstract;
+    }
+
+    public setSelectedArticle(article: ArticleType): void {
+      this.selectedArticle = article;
+    }
+
+    public unsetSelectedArticle(article: ArticleType) {
+      if (this.selectedArticle?.title == article.title) {
+        this.selectedArticle = null;
+      }
+    }
+
+    public markAsRead(article: ArticleType, index: number) {
+      this.readArticles.push(article);
+
+      this.unreadArticles.splice(index, 1);
+    }
   }
 
   // export default Vue.extend({
