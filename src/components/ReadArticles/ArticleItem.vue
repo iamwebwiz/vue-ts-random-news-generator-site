2fb13f56c36143d3a4488ff3ba40a9f09f458dfe
Ezekiel <iamwebwiz@gmail.com>
2020-09-12 23:21:44 +0100

    add components for read articles

---
+++ src/components/ReadArticles/ArticleItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div
+      class="bg-green-100 px-6 py-4 rounded shadow cursor-pointer"
+      @click="toggleShowArticleAbstract(article)"
+    >
+      <div class="flex justify-between">
+        <h2 class="font-semibold text-sm sm:text-base capitalize text-gray-800">
+          {{ article.title }}
+        </h2>
+        <a
+          :href="article.url"
+          class="text-gray-500 text-sm uppercase hover:text-red-500"
+          target="_blank"
+          >Link</a
+        >
+      </div>
+      <div
+        v-if="article.showAbstract"
+        class="bg-gray-300 p-4 rounded-md mt-6 font-light text-sm"
+      >
+        {{ article.abstract }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  interface ArticleType {
+    title: string;
+    url: string;
+    abstract: string;
+    showAbstract: boolean;
+  }
+
+  export default Vue.extend({
+    name: "ArticleItem",
+    props: {
+      article: {
+        type: Object as () => ArticleType,
+        required: true,
+      },
+      toggleShowArticleAbstract: {
+        required: true,
+      },
+    },
+  });
+</script>
---
+++ src/components/ReadArticles/Index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div v-if="articles.length > 0" class="my-6">
+      <h2 class="text-sm sm:text-lg lg:text-2xl capitalize">
+        Read articles:
+      </h2>
+      <div
+        class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 my-4"
+      >
+        <div v-for="(article, index) in articles" :key="index">
+          <article-item
+            :article="article"
+            :index="index"
+            :toggleShowArticleAbstract="toggleShowArticleAbstract"
+          ></article-item>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <h4 class="text-center text-gray-700 my-10">
+        You have not read any article yet.
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+  import ArticleItem from "./ArticleItem.vue";
+
+  interface ArticleType {
+    title: string;
+    url: string;
+    abstract: string;
+    showAbstract: boolean;
+  }
+
+  export default Vue.extend({
+    name: "ReadArticles",
+    components: {
+      ArticleItem,
+    },
+    props: {
+      articles: {
+        type: Array as () => Array<ArticleType>,
+        required: true,
+      },
+      toggleShowArticleAbstract: {
+        required: true,
+      },
+    },
+  });
+</script>
